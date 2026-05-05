bb8d4333e0d6f794132579ff3d7a2ee820688187
Alexndre Militao <ale.levi@hotmail.com>
2026-05-05 14:28:22 -0300

    removendo linux

--- lib/controller/localization_manager.dart
+++ lib/controller/localization_manager.dart
@@ -9,44 +9,16 @@ class LocalizationManager with ChangeNotifier {
 
   static const String defaultLanguageCode = "en";
 
-  final Map<String, Map<String, String>> _mapLanguages = {
-    "pt": {
-      "clearBooksText": "Limpar todos os livros",
-      "languageText": "Idioma",
-      "clearButton": "Limpar",
-      "defaultDeviceLanguageItem": "Do dispositivo",
-      "homeTitle": "Grimório",
-      "homeEmpty": "Seu Grimório está vazio",
-      "homeEmptyCall": "Vamos aprender algo novo?"
-    },
-    "en": {
-      "clearBooksText": "Clear all books",
-      "languageText": "Languages",
-      "clearButton": "Erase Books",
-      "defaultDeviceLanguageItem": "From device",
-      "homeTitle": "Spellbook",
-      "homeEmpty": "Your spellbook is empty",
-      "homeEmptyCall": "Let's learn something new?"
-    },
-    "es": {
-      "clearBooksText": "Eliminar todos los libros",
-      "languageText": "Idioma",
-      "clearButton": "Limpiar",
-      "defaultDeviceLanguageItem": "Del dispositivo",
-      "homeTitle": "Grimorio",
-      "homeEmpty": "Tu Grimorio está vacío",
-      "homeEmptyCall": "¿Aprendemos algo nuevo?"
-    },
-  };
+  final Map<String, Map<String, String>> _mapLanguages = {};
 
   Future<void> setLanguage(String newCode) async {
-    await _getLanguageFromServer(newCode);
+    await getLanguageFromServer(newCode);
     languageCode = newCode;
 
     notifyListeners();
   }
 
-  Future<void> _getLanguageFromServer(String newCode) async {
+  Future<void> getLanguageFromServer(String newCode) async {
     try {
       String url =
           "https://gist.githubusercontent.com/AlexandreMilitao/3a5aa7e0dd859a81d9ace7ccca57b250/raw/b941b7a3563c3f5702ce1474d6a1a1b1505ac6b7/app_$newCode.json";
